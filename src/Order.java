217c43ae914b90508011ba28135a8d85d76fa2b4
Joy <zhixualiu4-c@my.cityu.edu.hk>
2023-10-19 22:07:23 +0800

    modify order

--- src/Order.java
+++ src/Order.java
@@ -3,28 +3,38 @@ import java.util.List;
 
 public class Order {
     private int orderCode;
-    private List<Dish> cuisines;
-
+    private List<Dish> dishes;
+    private double distance;
+    private int status;
+    //0: 已下单但未送出
+    //1: 已下单且已经送出
+    //2：已经下单且客人已收到
+    //-1: 订单已取消
     public Order(){
     }
 
-    public Order(int orderCode, List<Dish> cuisines){
+    public Order(int orderCode, List<Dish> dishes, double distance){
         this.orderCode = orderCode;
-        this.cuisines = cuisines;
+        this.dishes = dishes;
+        this.distance = distance;
+        this.status = 0;
     }
 
     public int getOrderCode() { return orderCode; }
     public void setOrderCode(int orderCode){
         this.orderCode = orderCode;
     }
-    public List<Dish> getCuisines() { return cuisines; }
-    public void setCuisines(List<Dish> cuisines){
-        this.cuisines = cuisines;
+    public List<Dish> getDishes() { return dishes; }
+    public void setDishes(List<Dish> dishes){
+        this.dishes = dishes;
     }
-    public void addCuisine(Dish cuisine){
-        this.cuisines.add(cuisine);
+    public void addDishes(Dish dish){
+        this.dishes.add(dish);
     }
-    public void deleteCuisine(Dish cuisine){
-        this.cuisines.remove(cuisine);
-    }
+//    public void deleteDishes(Dish dish){
+//        this.dishes.remove(dish);
+//    }
+//    public void cancelOrder(){
+//        this.dishes.clear();
+//    }
 }
