a0d5ee7a9fa6f2a3698a8b320861b9bc8ccc2777
Joy <zhixualiu4-c@my.cityu.edu.hk>
2023-11-13 21:55:14 +0800

    add more input

--- src/main/java/com/cs3343/demo/core/Runner.java
+++ src/main/java/com/cs3343/demo/core/Runner.java
@@ -14,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
@@ -36,19 +37,31 @@ public class Runner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         int totalOrder = 0;
 
+
+
         ArrayList<Dish> dishes = Dish.inputDishInfo(DISH_INPUT);
         ArrayList<Cook> cooks = cook.inputCookInfo(COOK_INPUT);
 
         ArrayList<Order> orders = Order.inputOrderInfo(ORDER_INPUT, dishes);
 
 //        KitchenSchedule.testEarliestDishes(orders);
+        KitchenSchedule kitchenGen = KitchenSchedule.getGenerator();
+        kitchenGen.generateSchedule3_1(orders, cooks);
 
-        KitchenSchedule.generateSchedule3_1(orders, cooks);
         System.out.println("=====================================");
         for(Order o: orders){
             System.out.println("order "+o.getOrderCode()+" is ordered at "+o.getOrderTime()+", is finished cooking at "+o.getCookedTime()+". ");
         }
 
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Welcome to restaurant backend system!");
+//        System.out.println("Please enter the corresponding number to choose the function you want to use:");
+//        System.out.println("1 View/Change Setting");
+//        System.out.println("2 Input Orders");
+//
+//        String name = scanner.nextLine();
+//        System.out.println("Hello, " + name + "!");
+//        scanner.close();
 
 
     }
